b383f877231c969628cf34a36f2553fd04b24332
Lapidusa <113461954+Lapidusa@users.noreply.github.com>
2025-05-08 11:28:39 +0300

    requests for categories

--- composable/useCategory.ts
+++ composable/useCategory.ts
@@ -1,1 +1,57 @@
 import {useAuth} from "@composable/useAuth";
+import {useApi} from "@composable/api";
+import type {Category, CategoryRequest} from "@interfaces/category";
+
+export const useCategory = () => {
+  const api = useApi();
+  const Auth = useAuth();
+
+  const getCategory = async (categoryId: string) => {
+    const res = await api.get(`/category/get-category/${categoryId}`)
+    return res.data;
+  }
+
+  const getAllCategories = async () => {
+    const res = await api.get(`/category/all-categories`);
+    return res.data;
+  }
+
+  const getCategoryByStoreId = async (storeId: string) => {
+    const res = await api.get(`category/get-category-by-store/${storeId}`);
+    return res.data;
+  }
+
+  const swapPositionCategories = async (firstCategory: string, secondCategory: string) => {
+    const token = Auth.getToken();
+    if (!token && firstCategory && secondCategory) return
+    const res = await api.post(`category/swap-position/`,{
+      first_category: firstCategory,
+      second_category: secondCategory
+    },
+      {headers: {token}
+    });
+    return res.data;
+  }
+
+  const createCategory = async (category: CategoryRequest) => {
+    const token = Auth.getToken();
+    if (!token && category) return
+    const res = await api.post(`category/create-category`, category, {headers: {token}});
+    return res.data;
+  }
+
+  const updateCategory = async (category: Category) => {
+    const token = Auth.getToken();
+    if (!token && category) return
+    const res = await api.put(`category/${category.id}`, category, {headers: {token}});
+    return res.data;
+  }
+
+  const deleteCategory = async (categoryId: string) => {
+    const token = Auth.getToken();
+    if (!token && categoryId) return
+    const res = await api.delete(`category/${categoryId}`);
+    return res.data;
+  }
+  return {getCategory, getAllCategories, getCategoryByStoreId, swapPositionCategories, createCategory, updateCategory, deleteCategory}
+}
