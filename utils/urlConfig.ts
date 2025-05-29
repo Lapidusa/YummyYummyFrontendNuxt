3b8bf5e46523c6766cf3493274d6aab50c7c59ad
Diana <113461954+Lapidusa@users.noreply.github.com>
2025-05-29 15:52:23 +0300

    urlConfig debug

--- utils/urlConfig.ts
+++ utils/urlConfig.ts
@@ -1,7 +1,7 @@
 export const listUrl = (process.env.NODE_ENV === 'production')
   ? {
-    apiUrl:  'https://api.yummypizzayummy.ru/',
-    baseUrl: 'https://yummypizzayummy.ru/'
+    apiUrl:  'http://http://31.129.45.84:8000',
+    baseUrl: 'http://31.129.45.84:3000'
   }
   : {
     apiUrl:  'http://127.0.0.1:8000',
